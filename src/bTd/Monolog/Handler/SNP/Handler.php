3754facf9656c8923586c54082ea8890a7634e28
Peter Nerád <nerad.peter@gmail.com>
2018-09-27 20:30:14 +0200

    Initial commmit

--- src/bTd/Monolog/Handler/SNP/Handler.php
+++ src/bTd/Monolog/Handler/SNP/Handler.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * Copyright (c) 2018. Peter Nerád
+ * Project: SNPHandler
+ *
+ * @author    Peter Nerád <nerad.peter@gmail.com>
+ * @copyright 2018 Peter Nerád
+ * @license   https://opensource.org/licenses/MIT MIT
  */
 
 namespace bTd\Monolog\Handler\SNP;
@@ -29,15 +33,15 @@ class Handler extends AbstractProcessingHandler
      * Handler constructor
      *
      * @param Client $client
-     * @param int $level
-     * @param bool $bubble
+     * @param int    $level
+     * @param bool   $bubble
      * @param string $appTitle
      * @param string $appID
      * @param string $icon
      *
      * @throws \bTd\SNP\Protocol\Message\Response\ErrorResponseException
      */
-    public function __construct(Client $client, $level = Logger::DEBUG, bool $bubble = true, string $appTitle = "Monolog Logger", string $appID = "monolog", string $icon = "stock:platform-beos")
+    public function __construct(Client $client, $level=Logger::DEBUG, bool $bubble=true, string $appTitle="Monolog Logger", string $appID="monolog", string $icon="stock:platform-beos")
     {
         $this->client = $client;
         $this->client->register(new RegisterRequest($appID, $appTitle, $icon));
@@ -54,49 +58,49 @@ class Handler extends AbstractProcessingHandler
      */
     protected function write(array $record): void
     {
-        $title = "[ " . $record['datetime']->format("H:i:s") . " ] " . $record["channel"] . " " . $record['level_name'];
+        $title   = "[ ".$record['datetime']->format("H:i:s")." ] ".$record["channel"]." ".$record['level_name'];
         $message = $record['message'];
         if (count($record['context']) > 0) {
-            $message .= " [" . implode(", ", $record['context']) . "]";
+            $message .= " [".implode(", ", $record['context'])."]";
         }
 
         if (count($record['extra']) > 0) {
-            $message .= " [" . implode(", ", $record['extra']) . "]";
+            $message .= " [".implode(", ", $record['extra'])."]";
         }
 
         switch ($record['level']) {
-            case Logger::DEBUG:
-                $duration = 5;
-                $priority = -2;
-                $icon = "stock:system-new";
-                break;
-            case Logger::INFO:
-            case Logger::NOTICE:
-                $duration = 10;
-                $priority = 0;
-                $icon = "stock:system-info";
-                break;
-            case Logger::WARNING:
-                $duration = 0;
-                $priority = 1;
-                $icon = "stock:system-critical";
-                break;
-            case Logger::ERROR:
-                $duration = 0;
-                $priority = 2;
-                $icon = "stock:system-denied";
-                break;
-            case Logger::CRITICAL:
-            case Logger::ALERT:
-                $duration = 0;
-                $priority = 2;
-                $icon = "stock:system-blocked";
-                break;
-            case Logger::EMERGENCY:
-                $duration = 0;
-                $priority = 2;
-                $icon = "stock:system-blocked";
-                break;
+        case Logger::DEBUG:
+            $duration = 5;
+            $priority = -2;
+            $icon     = "stock:system-new";
+            break;
+        case Logger::INFO:
+        case Logger::NOTICE:
+            $duration = 10;
+            $priority = 0;
+            $icon     = "stock:system-info";
+            break;
+        case Logger::WARNING:
+            $duration = 0;
+            $priority = 1;
+            $icon     = "stock:system-critical";
+            break;
+        case Logger::ERROR:
+            $duration = 0;
+            $priority = 2;
+            $icon     = "stock:system-denied";
+            break;
+        case Logger::CRITICAL:
+        case Logger::ALERT:
+            $duration = 0;
+            $priority = 2;
+            $icon     = "stock:system-blocked";
+            break;
+        case Logger::EMERGENCY:
+            $duration = 0;
+            $priority = 2;
+            $icon     = "stock:system-blocked";
+            break;
         }//end switch
 
         $notify = new NotifyRequest($title, $message, $icon, $duration, $priority);
